b9d8a1f488577cf0fa66bd55174262d4fb0a08e8
jacksont432 <tommy.jackson@ndsu.edu>
2022-05-10 09:05:20 -0500

    Adding hello world to the project

---
+++ hello_world_python.py
@@ -0,0 +1,12 @@
+# This is how you do hello world in python
+# lines that start with # are comments
+# use print() to print something to the console
+# use '' or "" and place your message inside the quotes
+# def main is where the function is stored
+
+def main():
+    print('Hello World!')
+
+# this is where we call the function to print Hello World! to the console
+main()
+
